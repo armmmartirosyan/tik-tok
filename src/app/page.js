f8765be7298d94b070148c5927b145d2a0dd9ad2
wantitcodeit <front7226@gmail.com>
2025-08-10 16:43:19 +0400

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,30 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          alert(
+            JSON.stringify({
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            })
+          );
+        },
+        (err) => {
+          alert(err.message);
+        }
+      );
+    } else {
+      alert(JSON.stringify("Geolocation is not supported by this browser."));
+    }
+  }, []);
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
